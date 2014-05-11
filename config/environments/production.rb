235f952d52253f460aae3e587d776053e520ca12
shankpaul <shanpaul06@gmail.com>
2014-05-11 20:04:10 +0530

    front-end designs added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Kuttimama::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += ['active_admin.css']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Kuttimama::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :posts do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'posts#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
