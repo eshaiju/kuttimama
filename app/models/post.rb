7821cd09b29266da0f21fc9c5febaed8d0f22399
shankpaul <shanpaul06@gmail.com>
2014-05-11 21:30:31 +0530

    posts fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :movie
   belongs_to :category
   belongs_to :tag
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" , :small => "38x38"},:default_url => ActionController::Base.helpers.asset_path("user_default.png", type: :image)
+  has_attached_file :image, :styles => { :medium => "300x170>", :thumb => "100x100>" , :small => "38x38"},:default_url => ActionController::Base.helpers.asset_path("user_default.png", type: :image)
   
 end
