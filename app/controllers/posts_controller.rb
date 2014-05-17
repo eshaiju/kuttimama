b269e681706016cc52588622d2c7a32cba0ba7fe
eshaiju <eshaiju@gmail.com>
2014-05-18 03:28:48 +0530

    added searching in posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,30 @@
 class PostsController < ApplicationController
 
 	def index		
-		if params['q']
-			page = params[:page] || 1
-			# @posts = Sunspot.search(Post) do	
-			#   fulltext params['q']
-			#   with :category_id, params['category'] unless params['category'].nil?
-			#   paginate :page => page, :per_page => 9			  
-			# end.results
-			@posts = Post.page(params[:page]).per(9)
-		else
-			@posts = Post.page(params[:page]).per(9)
+		# if params['q']
+		# 	page = params[:page] || 1
+		# 	@posts = Post.page(params[:page]).per(9)
+		# else 
+		# 	@posts = Post.page(params[:page]).per(9)
+		# end
+		if params[:q]
+			p @posts = Post.where("title like '%#{params[:q]}%'")
 		end
+		if params[:category]
+			p @posts = Post.all if @posts.blank?
+			p @posts = @posts.where(:catagory_id =>params[:catagory])
+		end
+		if params[:q].blank? and params[:category].blank?
+			p @posts = Post.all 
+		end
+		@posts = @posts.page(params[:page]).per(9)
+
+		@catagories = Category.all
 		respond_to do |format|
 	      format.html
 	      format.js
 	    end
+
 	end
 
 	def home
